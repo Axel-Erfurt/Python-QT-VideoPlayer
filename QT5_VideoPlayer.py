702e556c21399874d1d0443c30b8922c97e6c37f
Axel Schneider <axel99092@t-online.de>
2016-12-07 05:37:16 +0100

    Update QT5_VideoPlayer.py

--- QT5_VideoPlayer.py
+++ QT5_VideoPlayer.py
@@ -1,24 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 from PyQt5.QtGui import QPalette, QKeySequence, QIcon
-from PyQt5 import QtWidgets
-from PyQt5 import QtCore
-from PyQt5.QtCore import QDir, Qt, QUrl, QSize, QPoint
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QDir, Qt, QUrl, QSize, QPoint, QTime
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel, 
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLineEdit,
         					QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout,  
-							QWidget, QShortcut, QMessageBox, QSystemTrayIcon)
+							QWidget, QShortcut)
+import os
+import gi
+gi.require_version('Gtk', '2.0')
+from gi.repository import Gtk as gtk
 
 class VideoPlayer(QWidget):
 
-    def __init__(self, parent=None):
-        super(VideoPlayer, self).__init__(parent)
+    def __init__(self):
+        super(VideoPlayer, self).__init__()
 
         self.setAttribute( Qt.WA_NoSystemBackground, True )
 
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.mediaPlayer.setVolume(80)
-        videoWidget = QVideoWidget()
+        self.videoWidget = QVideoWidget(self)
+        
+        self.lbl = QLineEdit('00:00:00')
+        self.lbl.setFixedWidth(60)
+        self.lbl.setUpdatesEnabled(True)
+        self.lbl.setStyleSheet(stylesheet(self))
+        
+        self.elbl = QLineEdit('00:00:00')
+        self.elbl.setFixedWidth(60)
+        self.elbl.setUpdatesEnabled(True)
+        self.elbl.setStyleSheet(stylesheet(self))
 
         self.playButton = QPushButton()
         self.playButton.setEnabled(False)
@@ -27,24 +41,32 @@ class VideoPlayer(QWidget):
         self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self.playButton.clicked.connect(self.play)
 
-        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider = QSlider(Qt.Horizontal, self)
         self.positionSlider.setStyleSheet (stylesheet(self)) 
-#        self.positionSlider.setFixedHeight(20)
-        self.positionSlider.setRange(0, 0)
+        self.positionSlider.setRange(0, 100)
         self.positionSlider.sliderMoved.connect(self.setPosition)
+        self.positionSlider.sliderMoved.connect(self.handleLabel)
+        self.positionSlider.setSingleStep(2)
+        self.positionSlider.setPageStep(20)
         self.positionSlider.setAttribute(Qt.WA_TranslucentBackground, True)
-
+        
         controlLayout = QHBoxLayout()
         controlLayout.setContentsMargins(5, 0, 5, 0)
         controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.lbl)
         controlLayout.addWidget(self.positionSlider)
+        controlLayout.addWidget(self.elbl)
 
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(videoWidget)
+        layout.addWidget(self.videoWidget)
         layout.addLayout(controlLayout)
 
         self.setLayout(layout)
+        
+        self.myinfo = "©2016Axel Schneider\n\nMouse Wheel = Zoom\nUP = Volume Up\nDOWN = Volume Down\n" + \
+				"LEFT = < 1 Minute\nRIGHT = > 1 Minute\n" + \
+				"SHIFT+LEFT = < 10 Minutes\nSHIFT+RIGHT = > 10 Minutes"
 		
 		#### shortcuts ####
         self.shortcut = QShortcut(QKeySequence("q"), self)
@@ -67,16 +89,15 @@ class VideoPlayer(QWidget):
         self.shortcut.activated.connect(self.volumeUp)
         self.shortcut = QShortcut(QKeySequence(Qt.Key_Down), self)
         self.shortcut.activated.connect(self.volumeDown)	
-
         self.shortcut = QShortcut(QKeySequence(Qt.ShiftModifier +  Qt.Key_Right) , self)
         self.shortcut.activated.connect(self.forwardSlider10)
-
         self.shortcut = QShortcut(QKeySequence(Qt.ShiftModifier +  Qt.Key_Left) , self)
         self.shortcut.activated.connect(self.backSlider10)
 
-        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
         self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
         self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.positionChanged.connect(self.handleLabel)
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
 
@@ -84,7 +105,7 @@ class VideoPlayer(QWidget):
 
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
-                '/Axel_1/Filme', "Videos (*.mp4 *.ts *.avi *.mpeg *.mpg *.mkv)")
+                QDir.homePath(), "Videos (*.mp4 *.ts *.avi *.mpeg *.mpg *.mkv *.VOB *.m4v)")
 
         if fileName != '':
             self.loadFilm(fileName)
@@ -98,13 +119,14 @@ class VideoPlayer(QWidget):
         self.playButton.setEnabled(True)
         self.mediaPlayer.play()
         self.hideSlider()
+        print(myurl)
 
     def play(self):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.mediaPlayer.pause()
         else:
             self.mediaPlayer.play()
-
+    
     def mediaStateChanged(self, state):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.playButton.setIcon(
@@ -115,9 +137,12 @@ class VideoPlayer(QWidget):
 
     def positionChanged(self, position):
         self.positionSlider.setValue(position)
-
+        
     def durationChanged(self, duration):
         self.positionSlider.setRange(0, duration)
+        mtime = QTime(0,0,0,0)
+        mtime = mtime.addMSecs(self.mediaPlayer.duration())
+        self.elbl.setText(mtime.toString())
 
     def setPosition(self, position):
         self.mediaPlayer.setPosition(position)
@@ -134,32 +159,54 @@ class VideoPlayer(QWidget):
     def contextMenuRequested(self,point):
         menu = QtWidgets.QMenu()
         actionFile = menu.addAction("open File (o)")
-        action3 = menu.addSeparator() 
+        actionclipboard = menu.addSeparator() 
         actionURL = menu.addAction("URL from Clipboard (u)")
-        action3 = menu.addSeparator() 
+        actionclipboard = menu.addSeparator() 
         actionToggle = menu.addAction("show / hide Slider (s)") 
         actionFull = menu.addAction("Fullscreen (f)")
-        action8 = menu.addSeparator()
+        action169 = menu.addAction("16 : 9")
+        action43 = menu.addAction("4 : 3")
+        actionSep = menu.addSeparator()
         actionInfo = menu.addAction("Info (i)")
         action5 = menu.addSeparator() 
         actionQuit = menu.addAction("Exit (q)") 
+
         actionFile.triggered.connect(self.openFile)
         actionQuit.triggered.connect(self.handleQuit)
         actionFull.triggered.connect(self.handleFullscreen)
         actionInfo.triggered.connect(self.handleInfo)
         actionToggle.triggered.connect(self.toggleSlider)
         actionURL.triggered.connect(self.playFromURL)
+        action169.triggered.connect(self.screen169)
+        action43.triggered.connect(self.screen43)
         menu.exec_(self.mapToGlobal(point))
-	
+
     def wheelEvent(self,event):
-        if self.positionSlider.isVisible():
-            self.setGeometry(self.frameGeometry().left(), self.frameGeometry().top(), \
-	        self.frameGeometry().width() \
-	        + event.angleDelta().y()/10.00, self.frameGeometry().width()/1.55)
+        mwidth = self.frameGeometry().width()
+        mheight = self.frameGeometry().height()
+        mleft = self.frameGeometry().left()
+        mtop = self.frameGeometry().top()
+        mscale = event.angleDelta().y() / 5
+        if mwidth / mheight > 1.4:
+            self.setGeometry(mleft, mtop, mwidth + mscale, (mwidth + mscale) / 1.778) 
         else:
-            self.setGeometry(self.frameGeometry().left(), self.frameGeometry().top(), \
-	        self.frameGeometry().width() \
-	        + event.angleDelta().y()/10.00, self.frameGeometry().width()/1.778)
+            self.setGeometry(mleft, mtop, mwidth + mscale, (mwidth + mscale) / 1.33)            
+
+    def screen169(self):
+        mwidth = self.frameGeometry().width()
+        mheight = self.frameGeometry().height()
+        mleft = self.frameGeometry().left()
+        mtop = self.frameGeometry().top()
+        mratio = 1.778
+        self.setGeometry(mleft, mtop, mwidth, mwidth / mratio)
+
+    def screen43(self):
+        mwidth = self.frameGeometry().width()
+        mheight = self.frameGeometry().height()
+        mleft = self.frameGeometry().left()
+        mtop = self.frameGeometry().top()
+        mratio = 1.33
+        self.setGeometry(mleft, mtop, mwidth, mwidth / mratio)
 
     def handleFullscreen(self):
         if self.windowState() & QtCore.Qt.WindowFullScreen:
@@ -170,28 +217,20 @@ class VideoPlayer(QWidget):
             print("Fullscreen entered")
 
     def handleInfo(self):
-            msg = QMessageBox()
-            msg.setStyleSheet('QMessageBox \
-				{background-color: darkgoldenrod;  border: 2px solid grey; } \
-				\nQLabel{color: black;} \
-				\nQPushButton{border: 2px solid grey; \
-				color: black; font-size: 12px; \
-				background-color: goldenrod; \
-				border-radius: 4px; padding: 6px; \
-				text-align: center;} \
-				\n QPushButton:hover{background-color: gold; color: black;}')
-            msg.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-            msg.setGeometry(self.frameGeometry().left() + 30, self.frameGeometry().top() + 30, 300, 400)
-            #msg.setIcon(QMessageBox.Information)
-            msg.setText("\tAxel Schneider\t\n\n\tPython with Qt5\t")
-            msg.setInformativeText("\t©2016\t")
-            msg.setWindowTitle("Phonon Player")
-            msg.setDetailedText("Mouse Wheel = Zoom\nUP = Volume Up\nDOWN = Volume Down\n" + \
-				"LEFT = < 1 Minute\nRIGHT = > 1 Minute\n" + \
-				"SHIFT+LEFT = < 10 Minutes\nSHIFT+RIGHT = > 10 Minutes")
-            msg.setStandardButtons(QMessageBox.Close)
-            msg.exec()
-	
+        mwidth = self.frameGeometry().width()
+        mheight = self.frameGeometry().height()
+        mleft = self.frameGeometry().left()
+        mtop = self.frameGeometry().top()
+############################## dialog box ###################
+        dialog = gtk.MessageDialog(None, 0, 0,
+        gtk.ButtonsType.CLOSE, "QT5 Player")
+        dialog.format_secondary_text(self.myinfo)
+        dialog.set_keep_above(True)
+        dialog.set_decorated(False)
+        ret = dialog.run()
+        if ret == gtk.ResponseType.CLOSE:
+            dialog.destroy()
+#############################################################
     def toggleSlider(self):	
         if self.positionSlider.isVisible():
             self.hideSlider()
@@ -199,12 +238,17 @@ class VideoPlayer(QWidget):
             self.showSlider()
 	
     def hideSlider(self):
+            self.lbl.hide()
+            self.elbl.hide()
             self.positionSlider.hide()
             self.playButton.hide()
             self.setGeometry(self.frameGeometry().left(), self.frameGeometry().top(), \
 			self.frameGeometry().width(), self.frameGeometry().width()/1.778)
 	
-    def showSlider(self):	
+    def showSlider(self):
+            self.lbl.show()
+            self.elbl.show()
+            self.lbl.update()
             self.positionSlider.show()
             self.playButton.show()
             self.setGeometry(self.frameGeometry().left(), self.frameGeometry().top(), \
@@ -212,19 +256,15 @@ class VideoPlayer(QWidget):
 	
     def forwardSlider(self):
         self.mediaPlayer.setPosition(self.mediaPlayer.position() + 100*60)
-        print("Position: " + ("%.2f" % (self.mediaPlayer.position()/100/60)))
 
     def forwardSlider10(self):
             self.mediaPlayer.setPosition(self.mediaPlayer.position() + 1000*60)
-            print("%.0f" %  (self.mediaPlayer.position()/1000/60))
 
     def backSlider(self):
         self.mediaPlayer.setPosition(self.mediaPlayer.position() - 100*60)
-        print("Position: " + ("%.2f" % (self.mediaPlayer.position()/100/60)))
 
     def backSlider10(self):
         self.mediaPlayer.setPosition(self.mediaPlayer.position() - 1000*60)
-        print("Position: " + ("%.2f" % (self.mediaPlayer.position()/1000/10)))
 		
     def volumeUp(self):
         self.mediaPlayer.setVolume(self.mediaPlayer.volume() + 10)
@@ -252,22 +292,36 @@ class VideoPlayer(QWidget):
         self.loadFilm(f)
 	
     def loadFilm(self, f):
+            self.hideSlider()
             self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(f)))
             self.playButton.setEnabled(True)
             self.mediaPlayer.play()
-            self.hideSlider()
+            print(self.mediaPlayer.media().canonicalResource().resolution())
+            print(self.mediaPlayer.metaData("Resolution"))
 	  
     def openFileAtStart(self, filelist):
             matching = [s for s in filelist if ".myformat" in s]
             if len(matching) > 0:
                 self.loadFilm(matching)
 
+###################################################################
+    def handleLabel(self):
+            self.lbl.clear()
+            mtime = QTime(0,0,0,0)
+            self.time = mtime.addMSecs(self.mediaPlayer.position())
+            self.lbl.setText(self.time.toString())
+            print(str(self.mediaPlayer.position()) \
+            + " - " + self.time.toString())
+            app.processEvents()
+
+###################################################################
+
 def stylesheet(self):
     return """
 
 QSlider::handle:horizontal 
 {
-background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #333, stop:1 #555);
+background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #333, stop:1 #555555);
 width: 14px;
 border-radius: 0px;
 }
@@ -275,25 +329,32 @@ border-radius: 0px;
 QSlider::groove:horizontal {
 border: 1px solid #444;
 height: 10px;
-background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #000, stop:1 #222);
+background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #000, stop:1 #222222);
+}
+
+QLineEdit
+{
+background: black;
+color: #585858;
+border: 0px solid #076100;
+font-size: 12px;
+font-weight: bold;
 }
     """
-
-##########################################
-##########################################
 
 if __name__ == '__main__':
 
     import sys
     app = QApplication(sys.argv)
+
     player = VideoPlayer()
     player.setAcceptDrops(True)
     player.setWindowTitle("QT5 Player")
     player.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-    player.setGeometry(10, 200, 400, 290)
+    player.setGeometry(100, 300, 400, 290)
     player.setContextMenuPolicy(QtCore.Qt.CustomContextMenu);
     player.customContextMenuRequested[QtCore.QPoint].connect(player.contextMenuRequested)
     player.show()
     player.hideSlider()
-
-    sys.exit(app.exec_())
+#    player.loadFilm('/Axel_1/Filme/maibock2016.mp4')
+sys.exit(app.exec_())
